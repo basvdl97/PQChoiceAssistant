e9d6661b2e9f1c20294a7754e666c375d25866e4
basvdl97 <32781551+basvdl97@users.noreply.github.com>
2024-10-17 14:48:44 +0200

    adding education graph files

---
+++ src/js/json/kem-education-graphs.js
@@ -0,0 +1,74 @@
+export default {
+    "Performance": {
+        "Level 1": {
+            "Key generation (cycles)": {
+                "BIKE": 636580,
+                "McEliece": 137345418,
+                "Frodo": 3353565,
+                "HQC": 104578,
+                "Kyber": 29523
+            },
+            "Encryption (cycles)": {
+                "BIKE": 110537,
+                "McEliece": 49004,
+                "Frodo": 3779649,
+                "HQC": 196742,
+                "Kyber": 37250
+            },
+            "Decryption (cycles)": {
+                "BIKE": 1750669,
+                "McEliece": 119810,
+                "Frodo": 3707032,
+                "HQC": 359751,
+                "Kyber": 27018
+            }
+        },
+        "Level 3": {
+            "Key generation (cycles)": {
+                "BIKE": 1892240,
+                "McEliece": 430364248,
+                "Frodo": 6880309,
+                "HQC": 244164,
+                "Kyber": 45796
+            },
+            "Encryption (cycles)": {
+                "BIKE": 250696,
+                "McEliece": 90886,
+                "Frodo": 7496346,
+                "HQC": 459679,
+                "Kyber": 54633
+            },
+            "Decryption (cycles)": {
+                "BIKE": 4312896,
+                "McEliece": 232302,
+                "Frodo": 7409202,
+                "HQC": 745706,
+                "Kyber": 42180
+            }
+        },
+        "Level 5": {
+            "Key generation (cycles)": {
+                "BIKE": 4535035,
+                "McEliece": 674012157,
+                "Frodo": 12337760,
+                "HQC": 446071,
+                "Kyber": 62056
+            },
+            "Encryption (cycles)": {
+                "BIKE": 504653,
+                "McEliece": 196153,
+                "Frodo": 13267931,
+                "HQC": 844470,
+                "Kyber": 74852
+            },
+            "Decryption (cycles)": {
+                "BIKE": 10382235,
+                "McEliece": 273109,
+                "Frodo": 13090622,
+                "HQC": 1409570,
+                "Kyber": 59944
+            }
+        }
+    },
+    "Size": {}
+}
